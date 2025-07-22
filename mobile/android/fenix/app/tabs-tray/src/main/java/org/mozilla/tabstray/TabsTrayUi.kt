6edb86a2c168296b58e9a102d33b5f9eff770c2f
Matthew Tighe <matthewdtighe@gmail.com>
2025-07-22 15:38:19 -0700

    wip

---
+++ mobile/android/fenix/app/tabs-tray/src/main/java/org/mozilla/tabstray/TabsTrayUi.kt
@@ -0,0 +1,11 @@
+package org.mozilla.tabstray
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+
+@Composable
+fun TabsTrayUi() {
+    Column {
+
+    }
+}
